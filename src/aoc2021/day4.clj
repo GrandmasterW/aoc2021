8da3f30db0c555597254db2b1578503737057c35
Werner Beckmann <werner.beckmann@gmail.com>
2021-12-18 11:50:18 +0100

    Day 4, part 1 done

---
+++ src/aoc2021/day4.clj
@@ -0,0 +1,127 @@
+(ns aoc2021.day4
+  (:require [clojure.string :as str]
+            [aoc2021.load :as ld]
+            [aoc2021.util :as ut]))
+
+
+;; giant squid
+
+
+(def day4-example-input "src/aoc2021/day4-example-input.txt")
+  (def day4-full-input "src/aoc2021/day4-full-input.txt")
+
+
+;; ----------------------------------------------------------------------
+;; loading data into structures
+;; ----------------------------------------------------------------------
+
+(defn raw-draw-to-vec
+  "Converts a string of <int>,<int>,... into a vector of int"
+  [line]
+  (mapv #(Integer/parseInt %1)
+       (str/split line #",")))
+
+(defn make-cell-from-s
+  "Create an initial cell for a string representation like '45' -> [45 false]"
+  [s]
+  [(Integer/parseInt s) false])
+
+(defn board-line-to-row
+  "Return a vector of cells for a string containing ints with blanks. Adds a false to each cells number"
+  [s]
+  (mapv make-cell-from-s
+        (remove empty? (str/split s #" "))))
+  
+(defn raw-to-board
+  "convert a sequence of subsequent strings with numbers to a single board. Returns a board as a vector of rows, each row being a vector of number and status."
+  [rb]
+  (mapv board-line-to-row rb))
+  
+(defn get-day4-instructions
+  "load into structure for bingo.
+  Beware: lines with spaces may lead to errors - filter them or remove them in data file!"
+  [filename]
+  (let [raw (ld/load-lines-to-vec filename)
+        drawvec (raw-draw-to-vec (first raw))
+        boards (->> (rest raw)
+                    (partition-by empty?)
+                    (remove (comp empty? first))
+                    (mapv raw-to-board))
+        ]
+    {:draws drawvec
+     :boards boards} ))
+
+
+
+;; ----------------------------------------------------------------------
+;; playing bingo with structures
+;; ----------------------------------------------------------------------
+
+(defn apply-number-to-row
+  "returns vector of cells, if a number equals n, than with true"
+  [n row]
+  (mapv #(if (= n (first %)) [n true] %) row))
+
+(defn apply-number-to-board
+  "one number, one board, set first occurance (if any) to true"
+  [n b]
+  ;; mapv assoc
+  (mapv (partial apply-number-to-row n) b))
+
+(defn row-all-done?
+  "true if all elements of the vector have value true"
+  [v]
+  (every? (comp true? second) v)) 
+
+;;(def every-vec-done? (partial every? row-all-done?))
+
+(defn board-bingo?
+  "true, if one column or row is fully done"
+  [b]
+  (or (some row-all-done? b)
+      (some row-all-done? (ut/transpose b))))
+
+(defn apply-draws
+  "subsequently apply bingo numbers to boards until first board is done. Return board and winning number. If full-mode, stop if all boards are done."
+  [bs] ; board-structure
+  (if (empty? (:draws bs)) bs ; done for bad... 
+      (let [draw (first (:draws bs))
+            new-boards (mapv (partial apply-number-to-board draw) (:boards bs))
+            finished-boards (filterv board-bingo? new-boards)
+            ]
+        (if  (some seq finished-boards)
+          {:draws (:draws bs)
+           :last-draw draw
+           :bingo (first finished-boards)
+           :boards new-boards}
+          ;; else recur with the rest and the new boards
+          (recur
+                 {:draws (rest (:draws bs))
+                  :boards new-boards})))))
+
+
+(defn sum-unmarked
+  "returns the sum of all unmarked numbers on a board"
+  [b]
+  (->> b
+       (apply concat)
+       (filter (comp false? second))
+       (map first)
+       (reduce +)))
+        
+;; ----------------------------------------------------------------------
+;; result computation
+;; ----------------------------------------------------------------------
+
+(defn day4a-result
+  "return multiplied results"
+  [bs]
+  (let [rs (apply-draws bs)
+        su (sum-unmarked (:bingo rs))]
+        (* su (:last-draw rs))))
+
+(defn day4b-result
+  "return multiplied results"
+  [bs]
+  nil)
+
---
+++ test/aoc2021/day4_test.clj
@@ -0,0 +1,182 @@
+(ns aoc2021.day4-test
+  (:require [clojure.test :refer :all]
+            [aoc2021.day4 :as d4]))
+
+
+(deftest raw-draw-to-string-test
+  (testing ""
+    (is (=
+        [1 2 3 9 8 7 -1]
+        (d4/raw-draw-to-vec "1,2,3,9,8,7,-1")))))
+
+(deftest board-line-to-row-test
+  (testing "converting simple line"
+    (is (=
+         [[1 false][10 false][20 false]]
+         (d4/board-line-to-row " 1 10 20")))))
+
+(deftest raw-to-board-test
+  (testing "conversion of sequence of strings to board"
+    (is (=
+         [[[22 false] [13 false] [17 false] [11 false] [0 false]]
+          [[8 false] [2 false] [23 false] [4 false] [24 false]]
+          [[21 false] [9 false] [14 false] [16 false] [7 false]]
+          [[6 false] [10 false] [3 false] [18 false] [5 false]]
+          [[1 false] [12 false] [20 false] [15 false] [19 false]]]
+         (d4/raw-to-board 
+          '("22 13 17 11  0"
+           " 8  2 23  4 24"
+           "21  9 14 16  7"
+           " 6 10  3 18  5"
+           " 1 12 20 15 19"))))))
+
+
+(def ex4res
+        {:draws
+         [7           4           9           5           11           17           23           2           0           14           21           24           10           16           13           6           15           25           12           22           18           20           8           19          3          26          1],
+         :boards
+         [[[[22 false] [13 false] [17 false] [11 false] [0 false]]
+           [[8 false] [2 false] [23 false] [4 false] [24 false]]
+           [[21 false] [9 false] [14 false] [16 false] [7 false]]
+           [[6 false] [10 false] [3 false] [18 false] [5 false]]
+           [[1 false] [12 false] [20 false] [15 false] [19 false]]]
+          [[[3 false] [15 false] [0 false] [2 false] [22 false]]
+           [[9 false] [18 false] [13 false] [17 false] [5 false]]
+           [[19 false] [8 false] [7 false] [25 false] [23 false]]
+           [[20 false] [11 false] [10 false] [24 false] [4 false]]
+           [[14 false] [21 false] [16 false] [12 false] [6 false]]]
+          [[[14 false] [21 false] [17 false] [24 false] [4 false]]
+           [[10 false] [16 false] [15 false] [9 false] [19 false]]
+           [[18 false] [8 false] [23 false] [26 false] [20 false]]
+           [[22 false] [11 false] [13 false] [6 false] [5 false]]
+           [[2 false] [0 false] [12 false] [3 false] [7 false]]]]}
+)  
+
+(deftest get-day4-instructions-test
+  (testing "loading example"
+    (is (= ex4res
+           (d4/get-day4-instructions d4/day4-example-input)))))
+
+;; ----------------------------------------------------------------------
+(deftest apply-number-to-row-test
+  (testing "simple setting a bingo number"
+    (is (= [[1 false][99 true][2 false]]
+           (d4/apply-number-to-row 99 [[1 false][99 false][2 false]])))
+    (is (= [[1 false][99 false][2 false]]
+           (d4/apply-number-to-row 100 [[1 false][99 false][2 false]])))
+    (is (= [[1 false][99 true][2 false]]
+           (d4/apply-number-to-row 99 [[1 false][99 true][2 false]])))
+    ))
+
+(deftest apply-number-to-board-test
+  (let [orig [ [[1 false][2 false][3 false]]
+               [[40 false][50 false][60 false]]
+               [[70 false][7 false][79 false]] ]  ]
+    (testing "setting one number in a simple board"
+      (is (=
+           [[[1 false][2 false][3 false]]
+            [[40 false][50 true][60 false]]
+            [[70 false][7 false][79 false]] ]
+           (d4/apply-number-to-board 50 orig))))))
+           
+(deftest row-all-done?-test
+  (testing "simple example of bingo row"
+    (is (= true
+           (d4/row-all-done?
+            [[1 true][2 true][3 true]]))))
+  (testing "simple example of bingo row"
+    (is (= false
+           (d4/row-all-done?
+            [[1 false][2 true][3 true]])))))
+
+
+(deftest board-bingo?-test
+  (testing "check if we find bingo rows or columns"
+    (is (= true
+           (d4/board-bingo? [[[1 false][4 false][7 false]]
+                             [[2 false][5 false][8 false]]
+                             [[3 true][6 true][9 true]]
+                             ])))
+        (is (= true
+           (d4/board-bingo? [[[1 true][4 false][7 false]]
+                             [[2 true][5 false][8 false]]
+                             [[3 true][6 false][9 false]]
+                             ]))))
+    (testing "check if we fail to find bingo rows or columns"
+      (is (not
+             (d4/board-bingo? [[[1 false][4 false][7 false]]
+                               [[2 false][5 false][8 false]]
+                               [[3 true][6 false][9 true]]
+                               ])))
+      (is (not
+             (d4/board-bingo? [[[1 false][4 false][7 false]]
+                               [[2 false][5 false][8 false]]
+                               [[3 false][6 false][9 false]]
+                               ])))))
+
+(deftest apply-draws-test
+  (testing "simple 2x2 example with two boards, find first one with first row"
+    (let [bs {:draws [1 2 5 3]
+              :boards
+              [
+               [[[1 false][2 false]]
+                [[3 false][4 false]]]
+               [[[3 false][4 false]]
+                [[5 false][6 false]]]
+               ]}
+          rs (d4/apply-draws bs) ]
+      (is (= 2 (:last-draw rs)))
+      (is (=   [[[1 true] [2 true]]
+                [[3 false][4 false]]]
+               (:bingo rs)))))
+  (testing "simple 2x2 example with two boards, find 2nd with first column"
+    (let [bs {:draws [3 5 6 1]
+              :boards
+              [
+               [[[1 false][2 false]]
+                [[3 false][4 false]]]
+               [[[3 false][4 false]]
+                [[5 false][6 false]]]
+               ]}
+          rs (d4/apply-draws bs) ]
+      (is (= 5 (:last-draw rs)))
+      (is (=   [[[3 true][4 false]]
+                [[5 true][6 false]]]
+               (:bingo rs)))))
+  (testing "full-mode: simple 2x2 example with two boards, find 2nd with first column"
+    (let [bs {:draws [3 5 1 2 6]
+              :boards
+              [
+               [[[1 false][2 false]]
+                [[3 false][4 false]]]
+               [[[3 false][4 false]]
+                [[5 false][6 false]]]
+               ]}
+          rs (d4/apply-draws bs) ]
+      (is (= 1 (:last-draw rs)))
+      (is (=   [[[3 true][4 false]]
+                [[5 true][6 false]]]
+               (:bingo rs)))))) ;; work cannot function correctly
+
+(deftest sum-unmarked-test
+  (testing "simple 2x2 example"
+    (is (= 3
+           (d4/sum-unmarked [[[1 false] [2 false]] [[3 true] [4 true]]])))))
+
+(deftest day4a-result-test
+  (testing "example-result"
+    (is (= 4512
+           (d4/day4a-result (d4/get-day4-instructions d4/day4-example-input)))))
+  (testing "puzzle input"
+    (is (= 51034
+           (d4/day4a-result (d4/get-day4-instructions d4/day4-full-input)))))
+  )
+
+(deftest day4b-result-test
+  (testing "example-result"
+    (is (= 1924
+           (d4/day4b-result (d4/get-day4-instructions d4/day4-example-input)))))
+  (testing "puzzle input"
+    (is (= 0
+           (d4/day4b-result (d4/get-day4-instructions d4/day4-full-input)))))
+  )
